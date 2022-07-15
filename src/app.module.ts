06e1e6a0b74c1f2e284d85e8060f9d2d67de9f72
chase <wowzerzchase@gmail.com>
2022-07-15 10:59:28 -0700

    github actions w/ serverless deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import * as typeOrmConfig from './typeorm.config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(typeOrmConfig),
-  ]
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
 })
 export class AppModule {}
