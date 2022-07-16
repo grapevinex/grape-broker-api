9c83f60fadaa37c777272ecc1685e6b353e47062
chase <wowzerzchase@gmail.com>
2022-07-16 13:10:58 -0400

    github actions w/ serverless deployment (#2)

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,22 +1,16 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const typeOrmConfig: TypeOrmModuleOptions = {
-    type: process.env.DB_TYPE as 'postgres',
-    host: process.env.POSTGRES_HOST,
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'alpaca',
-    synchronize: true,
-    entities: [
-        "src/entity/**/*{.js,.ts}"
-     ],
-    migrations: [
-        "src/migration/**/*{.js,.ts}"
-     ],
-     subscribers: [
-        "src/subscriber/**/*{.js,.ts}"
-     ],
-}
+  type: process.env.DB_TYPE as 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'alpaca',
+  synchronize: true,
+  entities: ['src/entity/**/*{.js,.ts}'],
+  migrations: ['src/migration/**/*{.js,.ts}'],
+  subscribers: ['src/subscriber/**/*{.js,.ts}'],
+};
 
-export = typeOrmConfig
+export = typeOrmConfig;
