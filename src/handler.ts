fb2c817705826a80d59f2cf37fb759eeabc5ffa6
tony-garand <anesthetics1@gmail.com>
2022-07-16 22:43:36 -0700

    adding types and linting

--- src/handler.ts
+++ src/handler.ts
@@ -1,49 +1,48 @@
-import { Handler, Context } from 'aws-lambda';
-import { Server } from 'http';
-import { createServer, proxy } from 'aws-serverless-express';
-import { eventContext } from 'aws-serverless-express/middleware';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
-import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { AppModule } from './app.module';
-import express from 'express';
+import { Handler, Context } from 'aws-lambda'
+import { Server } from 'http'
+import { createServer, proxy } from 'aws-serverless-express'
+import { eventContext } from 'aws-serverless-express/middleware'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { NestFactory } from '@nestjs/core'
+import { ExpressAdapter } from '@nestjs/platform-express'
+import { AppModule } from './app.module'
+import express from 'express'
 
 // NOTE: If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
 // due to a compressed response (e.g. gzip) which has not been handled correctly
 // by aws-serverless-express and/or API Gateway. Add the necessary MIME types to
 // binaryMimeTypes below
-const binaryMimeTypes: string[] = [];
+const binaryMimeTypes: string[] = []
 
-let cachedServer: Server;
+let cachedServer: Server
 
 async function bootstrapServer(): Promise<Server> {
   if (!cachedServer) {
-    const expressApp = express();
+    const expressApp = express()
     const nestApp = await NestFactory.create(
       AppModule,
       new ExpressAdapter(expressApp),
-    );
-    nestApp.setGlobalPrefix('api');
-    nestApp.use(eventContext());
-    nestApp.enableCors();
+    )
+    nestApp.setGlobalPrefix('api')
+    nestApp.use(eventContext())
+    nestApp.enableCors()
 
     const options = new DocumentBuilder()
       .addBearerAuth()
       .setTitle('Grape Broker API')
       .setVersion('1.0')
-      .build();
+      .build()
 
-    const document = SwaggerModule.createDocument(nestApp, options);
-    SwaggerModule.setup('api', nestApp, document);
+    const document = SwaggerModule.createDocument(nestApp, options)
+    SwaggerModule.setup('api', nestApp, document)
 
-    await nestApp.init();
-    cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+    await nestApp.init()
+    cachedServer = createServer(expressApp, undefined, binaryMimeTypes)
   }
-  return cachedServer;
+  return cachedServer
 }
 
 export const entry: Handler = async (event: any, context: Context) => {
-  cachedServer = await bootstrapServer();
-  return proxy(cachedServer, event, context, 'PROMISE').promise;
-};
+  cachedServer = await bootstrapServer()
+  return proxy(cachedServer, event, context, 'PROMISE').promise
+}
