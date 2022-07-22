aab60d5eb89d15b1c85c2f09ae0cdc1736cb333e
Tony Garand <anesthetics1@gmail.com>
2022-07-22 08:40:04 -0700

    Update clock.controller.ts

--- src/clock/clock.controller.ts
+++ src/clock/clock.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get} from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 import { ApiResponse, ApiTags } from '@nestjs/swagger'
 import { ClockDto } from './dto/clock.dto'
 import { ClockService } from './service/clock.service'
