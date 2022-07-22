e3f246becd24510936405e58addabc1e24c61908
Alex <alexgladin17@gmail.com>
2022-07-22 16:41:14 +0300

    Grape 10: Broker api clock (#12)
    * Grape-9: Add assets endpoints
    
    * Grape-8: Add http exception filter and http response interceptor
    
    * Grape-8: Lint code
    
    * Grape-10: Add clock API
    
    * Grape-10: Add .idea to gitignore
    
    * Grape-10: Add clock moodule
    
    * Delete .DS_Store
    
    * Update app.module.ts
    
    Co-authored-by: Tony Garand <anesthetics1@gmail.com>

--- src/alpaca/alpaca.module.ts
+++ src/alpaca/alpaca.module.ts
@@ -3,6 +3,7 @@ import { HttpModuleOptions } from '@nestjs/axios/dist/interfaces'
 import { HttpModule } from '@nestjs/axios'
 import { AlpacaAccountsService } from './service/alpaca.accounts.service'
 import { AlpacaAssetsService } from './service/alpaca.assets.service'
+import { AlpacaClockService } from './service/alpaca.clock.service'
 
 export interface AlpacaModuleOptions extends HttpModuleOptions {
   keyId: string
@@ -37,8 +38,12 @@ export class AlpacaModule {
           },
         }),
       ],
-      providers: [AlpacaAccountsService, AlpacaAssetsService],
-      exports: [AlpacaAccountsService, AlpacaAssetsService],
+      providers: [
+        AlpacaAccountsService,
+        AlpacaAssetsService,
+        AlpacaClockService,
+      ],
+      exports: [AlpacaAccountsService, AlpacaAssetsService, AlpacaClockService],
     }
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { ConfigModule } from '@nestjs/config'
 import { AccountsModule } from './accounts/accounts.module'
 import { AlpacaModule } from './alpaca/alpaca.module'
 import { AssetsModule } from './assets/assets.module'
+import { ClockModule } from './clock/clock.module'
+
 import typeOrmConfig from './typeorm.config'
 import alpacaModuleConfig from './alpaca.config'
 
@@ -14,6 +16,7 @@ import alpacaModuleConfig from './alpaca.config'
     AlpacaModule.register(alpacaModuleConfig()),
     AccountsModule,
     AssetsModule,
+    ClockModule,
   ],
   providers: [],
 })
